1d46ae868d28e1c22b54d204c06cb5f2525fcb8d
DenisZhi1 <159503111+DenisZhi1@users.noreply.github.com>
2026-03-17 20:49:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 // Простой конфиг без плагинов
 export default defineConfig({
 root: '.',
-base: '/TravelEnglish/', // ВАЖНО: имя репозитория c учётом регистра
+base: '/TravelEnglishTest/', // ВАЖНО: имя репозитория c учётом регистра
 });
